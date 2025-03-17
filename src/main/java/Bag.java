ecfddfd4bfd01ad8ce8d21fcde364d8233fbd369
김민석 <skyfox001015@naver.com>
2025-03-17 13:33:57 +0900

    feat : 티켓 문제 코드 1

---
+++ src/main/java/Bag.java
@@ -0,0 +1,32 @@
+public class Bag {
+    private Long amount;
+    private Ticket ticket;
+    private Invitation invitation;
+
+    public boolean hasInvitation(){
+        return invitation != null;
+    }
+    public boolean hasTicket(){
+        return ticket != null;
+    }
+
+    public void setTicket(Ticket ticket){
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount){
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount){
+        this.amount += amount;
+    }
+
+    public Bag(long amount){
+        this(null, amount);
+    }
+    public Bag(Invitation invitation, long amount) {
+        this.invitation = invitation;
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/TicketOffice.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class TicketOffice {
+    private Long amount;
+    private List<Ticket> tickets;
+
+    public TicketOffice(Long amount, Ticket... tickets){
+        this.amount = amount;
+        this.tickets.addAll(Arrays.asList(tickets));
+    }
+
+    public Ticket getTicket(){
+        return tickets.remove(0);
+    }
+
+    public void minusAmount(Long amount){
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount){
+        this.amount += amount;
+    }
+}
